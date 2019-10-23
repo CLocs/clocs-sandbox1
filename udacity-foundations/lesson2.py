5bdc35621d9e292255ae0bb78407823e73fcb82c
clocs <dascolin@gmail.com>
2019-10-22 22:07:14 -0400

    add: quick adding of tasks for Lesson 2

--- udacity-foundations/lesson2.py
+++ udacity-foundations/lesson2.py
@@ -1,8 +1,17 @@
 import webbrowser
+import time
 
 
 def web_stuff():
-    webbrowser.open('http://www.lawsofux.com')
+    ### Task 1
+    # webbrowser.open('http://www.lawsofux.com')
+
+    ### Task 2
+    # time.sleep(5)
+
+    ### Task 2
+    for i in range(3):
+        print('Take a break. Theres a little surprise before supper and it cannot wait.')
 
 
 if __name__ == '__main__':
