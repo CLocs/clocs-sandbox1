d7a12057edc2217f40189a179a8acd8982fdc836
clocs <dascolin@gmail.com>
2020-09-27 22:49:36 -0400

    tinkered with inputs

--- algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
+++ algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability.py
@@ -7,8 +7,32 @@ def reach(adj, x, y):
     return 0
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+    # input = sys.stdin.read()
+
+    # Hardcode inputs for ease
+
+    # Path exists, output->1
+    # input = [
+    #     4, 4,
+    #     1, 2,
+    #     3, 2,
+    #     4, 3,
+    #     1, 4,
+    #     1, 4
+    # ]
+    # print('Reachability should be: 1')
+
+    # Path doesn't exist, output->0
+    input = [
+        4, 2,
+        1, 2,
+        3, 2,
+        1, 4
+    ]
+    print('Reachability should be: 0')
+    # data = list(map(int, input))
+    data = input
+
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
@@ -18,4 +42,5 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    reachability = reach(adj, x, y)
+    print('Reachability = ', reachability)
