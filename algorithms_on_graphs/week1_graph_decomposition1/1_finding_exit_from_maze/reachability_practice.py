f9e71386c9360682176997f8838e20a6c2e05f83
clocs <dascolin@gmail.com>
2020-10-03 12:56:23 -0400

    prep for submission by reverting main of reachability.py

--- algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability_practice.py
+++ algorithms_on_graphs/week1_graph_decomposition1/1_finding_exit_from_maze/reachability_practice.py
@@ -24,9 +24,7 @@ def reach(adj, x, y):
         return 0
 
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
+def prep_inputs(data):
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
@@ -36,4 +34,41 @@ if __name__ == '__main__':
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
-    print(reach(adj, x, y))
+    return n, m, adj, x, y
+
+
+def _get_adjacent_vertices(adj, v):
+
+    pass
+
+if __name__ == '__main__':
+    # input = sys.stdin.read()
+
+    # Hardcode inputs for ease
+
+    # Path exists, output->1
+    input = [
+        4, 4,
+        1, 2,
+        3, 2,
+        4, 3,
+        1, 4,
+        1, 4
+    ]
+    print('Reachability should be: 1')
+
+    # # Path doesn't exist, output->0
+    # input = [
+    #     4, 2,
+    #     1, 2,
+    #     3, 2,
+    #     1, 4
+    # ]
+    # print('Reachability should be: 0')
+
+    # data = list(map(int, input))
+    data = input
+
+    n, m, adj, x, y = prep_inputs(data)
+    reachability = reach(adj, x, y)
+    print('Reachability = ', reachability)
