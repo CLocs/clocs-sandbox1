8daad0cc015d6193661a8d0d859a2d2019258b34
Colin Das <dascolin@gmail.com>
2024-08-03 19:44:12 -0400

    start and blocked

---
+++ real-estate/zillow-scrape.py
@@ -0,0 +1,30 @@
+import requests as req
+from bs4 import BeautifulSoup
+
+"""
+Notes
+pyzillow
+* https://pyzillow.readthedocs.io/en/latest/getting_started.html
+
+requires API key
+* https://www.zillowgroup.com/developers/
+
+
+"""
+
+def run_full_scraper():
+    # Address to Zillow link
+
+    # Get home type
+    # Get price
+    # Get number of bedrooms
+    # Get number of bathrooms
+    # Get number of garage spaces
+    # Get square footage
+    # Get travel time to TTC
+    # Get travel time to hospital
+    pass
+
+
+if __name__ == '__main__':
+    run_full_scraper()
